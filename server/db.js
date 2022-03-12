86160f0ea3abaa96bc8d606eb71c9eab9e685ac7
Harrison Hemstreet <harrisonhemstreet@gmail.com>
2022-03-11 18:03:33 -0700

    made significant progress on the server

--- server/db.js
+++ server/db.js
@@ -1,31 +1,39 @@
 const { Pool } = require('pg');
-require('dotenv').config();
+// require('dotenv').config();
 
-const pool = new Pool({
-    USER: process.env.USER,
-    PASSWORD: process.env.PASSWORD,
-    DATABASE: process.env.DATABASE,
-    HOST: process.env.HOST,
-    PORT: process.env.PORT
-});
+// const pool = new Pool({
+//     USER: process.env.USER,
+//     PASSWORD: process.env.PASSWORD,
+//     DATABASE: process.env.DATABASE,
+//     HOST: process.env.HOST,
+//     PORT: process.env.PORT
+// });
+
+const pool = new Pool();
 
 module.exports = {
     query: async (text, params = []) => {
-        if (params.length > 0) {
-            try {
-                await pool.query(text, params);
-            }
-            catch (e) {
-                console.error(e);
-            }
+        try {
+            return await pool.query(text, params);
+        } catch (err) {
+            return err.stack;
         }
-        else {
-            try {
-                await pool.query(text);
-            }
-            catch (e) {
-                console.error(e);
-            }
-        }
+        // if (params.length > 0) {
+        //     try {
+        //         await pool.query(text, params);
+        //     }
+        //     catch (e) {
+        //         console.error(e);
+        //     }
+        // }
+        // else {
+        //     try {
+        //         console.log('in query else try');
+        //         await pool.query(text);
+        //     }
+        //     catch (e) {
+        //         console.error(e);
+        //     }
+        // }
     },
 }
--- server/routes/appRoutes.js
+++ server/routes/appRoutes.js
@@ -4,23 +4,48 @@ const port = 3005;
 
 const db = require('../db');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
-
-app.get('/:id', async (req, res) => {
-    const query = {
-        text: "SELECT * FROM todos WHERE id = $1",
-        values: [req.params.id]
-    }
+app.get('/', async (req, res) => {
     try {
-        await db.query(query);
+        let result = await db.query("SELECT * FROM todo");
+        res.send(result.rows);
     }
     catch (e) {
         console.error(e);
     }
-})
+});
+
+app.get('/:id', async (req, res) => {
+    try {
+        let result = await db.query("SELECT * FROM todo WHERE id = $1", [req.params.id]);
+        res.send(result.rows);
+    }
+    catch (e) {
+        console.error(e);
+    }
+});
+
+app.post('/add/:task', async (req, res) => {
+    console.log('req', req.params);
+    try {
+        let result = await db.query("INSERT INTO todo (task, status) VALUES ($1, $2)", [req.params.task, 0]);
+        res.send(result.rows);
+    }
+    catch (e) {
+        console.error(e);
+    }
+});
+
+app.update('/updateName/:id/:name', async (req, res) => {
+    console.log('req', req.params);
+    try {
+        let result = await db.query("UPDATE todo SET `name` = $2 WHERE id = $1", [req.params.id, req.params.name]);
+        res.send(result.rows);
+    }
+    catch (e) {
+        console.error(e);
+    }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Todo app listening on port ${port}`);
 });
